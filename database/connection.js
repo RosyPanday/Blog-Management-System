bbe76e89fd4d4b3f35da7b7386ba8e13b91b6ff1
RosyPanday <pandayrosy14@gmail.com>
2025-11-18 18:32:21 +0545

    modify connection file to accomodate usersModel

--- database/connection.js
+++ database/connection.js
@@ -5,6 +5,7 @@ const CONNECTION_STRING= process.env.CONNECTION_STRING;
 const sequelize = new Sequelize(CONNECTION_STRING);
 
 const blogs=require('./models/blog.Model');
+const users=require('./models/user.Model');
 
 sequelize.authenticate()
 .then(()=>{
@@ -12,13 +13,16 @@ sequelize.authenticate()
 })
 .catch((err)=>{
    console.log("error",err);
-})
+});
 
+users.hasMany(blogs);
+blogs.belongsTo(users);
 //this db.blogs is used in controller to perform db operations
 const db={
     Sequelize:Sequelize,
     sequelize:sequelize,
     blogs:blogs(sequelize,DataTypes),
+    users:users(sequelize,DataTypes),
 }
  
 sequelize.sync({force:false, alter:true}).then(()=>{
