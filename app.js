339884bc1fbf8ec1de4e951160e9ac51c19950b9
RosyPanday <pandayrosy14@gmail.com>
2025-11-19 10:22:54 +0545

    added userroute in app.js

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ require("./database/connection");
 
 app.use(express.json());
 const blogRoutes=require('./routes/blog.route');
+const userRoutes=require('./routes/user.route');
 
-app.get('/',blogRoutes);
+app.use('/',blogRoutes);
+app.use('/',userRoutes);
+
+
 
 app.listen(3001,()=>{
      console.log("Server is running on port 3001 ");
